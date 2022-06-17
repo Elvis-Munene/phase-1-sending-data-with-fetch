d250e82f705a45f05310518bb80dce679081c73b
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-17 10:11:06 +0300

    commit tree 7d335b7f10fa5328b22b65645033d0ad19e16d1d

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData (userName, userEmail) {
+
+    let UserDetail = {
+        "name": userName,
+        "email": userEmail
+    }
+
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(UserDetail)
+    })
+    .then (response1 => response1.json())
+    .then (response2 => {
+        document.body.textContent = `${response2.id}`
+    })
+    .catch ((error) => {
+        //console.log(error)
+        console.log(error.message)
+        document.body.textContent = error.message
+})}
+
